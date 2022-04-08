2fc99fa63e6e94b782786b33072af32e51ee0c27
peterzky <378096232@qq.com>
2022-04-08 17:09:26 +0800

    initial commit

---
+++ default.nix
@@ -0,0 +1,27 @@
+{ emacsWithPackagesFromUsePackage, writeText, runCommand, emacsGit, emacs-src }:
+let
+  org-file = pkgs.writeText "default.org"
+    (builtins.readFile ../dotfiles/emacs/init.org);
+  emacs = emacsGit.overrideAttrs (_: rec {
+    src = emacs-src;
+  });
+
+  emacs-config = runCommand "emacs-config" { } ''
+    SITE_LISP=$out/share/emacs/site-lisp
+    mkdir -p $SITE_LISP
+    cp ${org-file} $SITE_LISP/default.org
+    cd $SITE_LISP
+    ${emacs}/bin/emacs -Q default.org --batch -f org-babel-tangle --kill
+    rm $SITE_LISP/default.org
+  '';
+
+in
+emacsWithPackagesFromUsePackage {
+  config = builtins.readFile "${emacs-config}/share/emacs/site-lisp/default.el";
+  package = emacs;
+  # override = pkgs.my-epkg-overrides;
+  extraEmacsPackages = epkgs: with epkgs; [
+    emacs-config
+  ];
+
+};
---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Peter's Emacs Flake";
+
+  inputs =
+    {
+      nixpkgs = {
+        url = "github:nixos/nixpkgs/master";
+      };
+
+      flake-utils.url = "github:numtide/flake-utils";
+
+      emacs-overlay = {
+        url = "github:nix-community/emacs-overlay";
+        inputs = { nixpkgs.follows = "nixpkgs"; };
+      };
+
+      emacs-src = {
+        url = "git+https:///mirrors.ustc.edu.cn/emacs.git";
+        flake = false;
+      };
+    };
+
+  outputs = { self, nixpkgs, flake-utils, emacs-overlay, emacs-src }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [ emacs-overlay.overlay ];
+          };
+        in
+        rec
+        {
+          defaultPackage = pkgs.callPackage ./default.nix { inherit emacs-src; };
+
+        }
+      );
+
+
